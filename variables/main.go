b5ee01bb190eb3622d5421747f7b0e8df095b43c
nathanpasca <nathandwi13@gmail.com>
2024-05-22 20:24:45 +0700

    learn-go-variables

--- variables/main.go
+++ variables/main.go
@@ -12,6 +12,14 @@ const PI = 3.14 // declare a CONSTANT variable, constant variable means that the
 
 // we declare the name of the constant variable usually using UPPERCASE, to differentiate between a normal variable and a constant variable
 
+var ( // We could make a multi function like this
+	hisAge int = 22
+	hisName string = "Resqi"
+)
+
+var a, b, c, d int = 1, 2, 3, 4 // or we could make a multiple function in one line
+var e, f, g, h = 1, 2, "test", 3.14 // we could make the multiple function of different data types, we dont define the data type.
+
 func main() {
 
 	myLastName := "Pasca" // more simpler way to define or declare a variable but it cannot be declared outside of function
